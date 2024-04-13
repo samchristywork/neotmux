50e7443b559a7ee0df9addb10b0352ef16d06eec
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-12 21:21:17 -0500

    feat: Add pty functions
    These functions are used to open and fork a new pty. These functions may
    not be portable.

--- src/ntmux.c
+++ src/ntmux.c
@@ -61,5 +61,100 @@ void ttySetRaw() {
   }
 }
 
+int ptyOpen(char *childName, size_t len) {
+  int fd = posix_openpt(O_RDWR | O_NOCTTY);
+  if (fd == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (grantpt(fd) == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (unlockpt(fd) == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  char *p = ptsname(fd);
+  if (p == NULL) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (strlen(p) < len) {
+    strncpy(childName, p, len);
+  } else {
+    exit(EXIT_FAILURE);
+  }
+
+  return fd;
+}
+
+pid_t ptyFork(int *parentFd, char *childName, size_t len,
+              const struct winsize *ws) {
+  char name[MAX_NAME];
+  int fd = ptyOpen(name, MAX_NAME);
+  if (fd == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (childName != NULL) {
+    if (strlen(name) < len) {
+      strncpy(childName, name, len);
+
+    } else {
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  pid_t childPid = fork();
+  if (childPid == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  if (childPid != 0) { // Parent
+    *parentFd = fd;
+    return childPid;
+  }
+
+  if (setsid() == -1) {
+    exit(EXIT_FAILURE);
+  }
+
+  close(fd);
+
+  {
+    int fd = open(name, O_RDWR);
+    if (fd == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (tcsetattr(fd, TCSANOW, &oldTermios) == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (ioctl(fd, TIOCSWINSZ, ws) == -1) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (dup2(fd, STDIN_FILENO) != STDIN_FILENO) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (dup2(fd, STDOUT_FILENO) != STDOUT_FILENO) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (dup2(fd, STDERR_FILENO) != STDERR_FILENO) {
+      exit(EXIT_FAILURE);
+    }
+
+    if (fd > STDERR_FILENO) {
+      close(fd);
+    }
+  }
+
+  return 0;
+}
+
 int main() {
 }
