437e20b9caa29a1165bd3d5e3f0c55208d0402c8
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-14 11:49:57 -0500

    feat: Render status bars
    The status bar can be on the top or the bottom of the window, and there
    is also an "info bar", which just displays debug info for now.

--- src/client.c
+++ src/client.c
@@ -86,12 +86,69 @@ void renderCell(int fd, VTermScreenCell cell) {
   }
 
   if (needsReset) {
-    printf("\033[0m");
+    write(fd, "\033[0m", 4);
   }
 }
 
-void renderScreen(Window *windows, int nWindows, int activeTerm, int rows, int cols) {
-  printf("\033[H"); // Move cursor to top left
+void infoBar(int fd, int rows, int cols) {
+  write(fd, "\033[7m", 4); // Invert colors
+  char buf[BUF_SIZE];
+  static int frame = 0;
+  int n = snprintf(buf, BUF_SIZE, "Rows: %d, Cols: %d, Frame: %d", rows, cols,
+                   frame);
+  frame++;
+  write(fd, buf, n);
+  write(fd, "\033[0m", 4);
+  write(fd, "\r\n", 2);
+}
+
+void color(int fd, int color) {
+  char buf[BUF_SIZE];
+  int n = snprintf(buf, BUF_SIZE, "\033[38;5;%dm", color);
+  write(fd, buf, n);
+}
+
+void statusBar(int fd, int cols) {
+  write(fd, "\033[7m", 4); // Invert colors
+  color(fd, 2);
+
+  int idx = 0;
+  char buf[cols];
+
+  char sessionName[] = "0";
+  int windowIndex = 0;
+  char windowName[] = "bash";
+
+  int n = snprintf(buf, cols, "[%s] ", sessionName);
+  write(fd, buf, n);
+  idx += n;
+
+  n = snprintf(buf, cols, "%d:%s*", windowIndex, windowName);
+  write(fd, buf, n);
+  idx += n;
+
+  char *statusRight = "Hello, World!";
+  idx += strlen(statusRight);
+
+  for (int i = idx; i < cols; i++) {
+    write(fd, " ", 1);
+  }
+
+  write(fd, statusRight, strlen(statusRight));
+
+  write(fd, "\033[0m", 4);
+  write(fd, "\r\n", 2);
+}
+
+void renderScreen(int fd, Window *windows, int nWindows, int activeTerm,
+                  int rows, int cols) {
+  write(fd, "\033[H", 3); // Move cursor to top left
+
+  infoBar(fd, rows, cols);
+
+  if (barPos == TOP) {
+    statusBar(fd, cols);
+  }
 
   VTermPos cursorPos;
   vterm_state_get_cursorpos(vterm_obtain_state(windows[activeTerm].vt),
@@ -102,13 +159,16 @@ void renderScreen(Window *windows, int nWindows, int activeTerm, int rows, int c
     for (int col = 0; col < cols; col++) {
       if (cursorPos.row == row - windows[activeTerm].row &&
           cursorPos.col == col - windows[activeTerm].col) {
-        printf("\033[7m");
+        write(fd, "\033[7m", 4); // Invert colors
       }
       bool isRendered = false;
       for (int k = 0; k < nWindows; k++) {
-        if (isInRect(row, col,
-              windows[k].row, windows[k].col, windows[k].height, windows[k].width
-              )) {
+        if (windows[k].closed) {
+          continue;
+        }
+
+        if (isInRect(row, col, windows[k].row, windows[k].col,
+                     windows[k].height, windows[k].width)) {
           VTermPos pos;
           pos.row = row - windows[k].row;
           pos.col = col - windows[k].col;
@@ -116,19 +176,23 @@ void renderScreen(Window *windows, int nWindows, int activeTerm, int rows, int c
 
           VTermScreenCell cell;
           vterm_screen_get_cell(vts, pos, &cell);
-          renderCell(cell);
+          renderCell(fd, cell);
           isRendered = true;
           break;
         }
       }
       if (!isRendered) {
-        printf("?");
+        write(fd, "?", 1);
       }
       if (cursorPos.row == row - windows[activeTerm].row &&
           cursorPos.col == col - windows[activeTerm].col) {
-        printf("\033[0m");
+        write(fd, "\033[0m", 4);
       }
     }
-    printf("\r\n");
+    write(fd, "\r\n", 2);
+  }
+
+  if (barPos == BOTTOM) {
+    statusBar(fd, cols);
   }
 }
