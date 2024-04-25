af3d94301f0542df481e8eff73e9472acda39eb0
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-24 21:53:42 -0500

    fix: Change the way layout is calculated
    The old layout algorithm would leave columns on the right of the screen.
    Now, we take up all available space.

--- src/layout.c
+++ src/layout.c
@@ -2,12 +2,14 @@
 #include <sys/ioctl.h>
 
 void evenVerticalLayout(Window *windows, int nWindows, int height, int width) {
+  int remaining = height - nWindows + 1;
   int row = 0;
+
   for (int i = 0; i < nWindows; i++) {
     windows[i].row = row;
     windows[i].col = 0;
-    windows[i].height = height / nWindows - 1;
-    windows[i].width = width;
+    windows[i].height = remaining / (nWindows - i);
+    windows[i].width = width - 1;
 
     vterm_set_size(windows[i].vt, windows[i].height, windows[i].width);
 
@@ -22,17 +24,20 @@ void evenVerticalLayout(Window *windows, int nWindows, int height, int width) {
     }
 
     row += windows[i].height + 1;
+    remaining -= windows[i].height;
   }
 }
 
 void evenHorizontalLayout(Window *windows, int nWindows, int height,
                           int width) {
+  int remaining = width - nWindows + 1;
   int col = 0;
+
   for (int i = 0; i < nWindows; i++) {
     windows[i].row = 0;
     windows[i].col = col;
     windows[i].height = height - 1;
-    windows[i].width = width / nWindows - 1;
+    windows[i].width = remaining / (nWindows - i);
 
     vterm_set_size(windows[i].vt, windows[i].height, windows[i].width);
 
@@ -47,6 +52,7 @@ void evenHorizontalLayout(Window *windows, int nWindows, int height,
     }
 
     col += windows[i].width + 1;
+    remaining -= windows[i].width;
   }
 }
 
