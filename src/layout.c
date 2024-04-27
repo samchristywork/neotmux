aeadb7388b4ff08256e12315941eec35e8ddef51
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-26 21:15:25 -0500

    refactor: Move code for updating layout into function
    This simplifies the layout calculation code, and makes it so that there
    is less duplicated code.

--- src/layout.c
+++ src/layout.c
@@ -1,16 +1,8 @@
 #include <layout.h>
 #include <sys/ioctl.h>
 
-void evenVerticalLayout(Pane *panes, int nPanes, int height, int width) {
-  int remaining = height - nPanes + 1;
-  int row = 0;
-
+void updateLayout(Pane *panes, int nPanes) {
   for (int i = 0; i < nPanes; i++) {
-    panes[i].row = row;
-    panes[i].col = 0;
-    panes[i].height = remaining / (nPanes - i);
-    panes[i].width = width - 1;
-
     vterm_set_size(panes[i].vt, panes[i].height, panes[i].width);
 
     struct winsize ws;
@@ -22,10 +14,24 @@ void evenVerticalLayout(Pane *panes, int nPanes, int height, int width) {
     if (ioctl(panes[i].process, TIOCSWINSZ, &ws) < 0) {
       exit(EXIT_FAILURE);
     }
+  }
+}
+
+void evenVerticalLayout(Pane *panes, int nPanes, int height, int width) {
+  int remaining = height - nPanes + 1;
+  int row = 0;
+
+  for (int i = 0; i < nPanes; i++) {
+    panes[i].row = row;
+    panes[i].col = 0;
+    panes[i].height = remaining / (nPanes - i);
+    panes[i].width = width;
 
     row += panes[i].height + 1;
     remaining -= panes[i].height;
   }
+
+  updateLayout(panes, nPanes);
 }
 
 void evenHorizontalLayout(Pane *panes, int nPanes, int height, int width) {
@@ -38,21 +44,11 @@ void evenHorizontalLayout(Pane *panes, int nPanes, int height, int width) {
     panes[i].height = height;
     panes[i].width = remaining / (nPanes - i);
 
-    vterm_set_size(panes[i].vt, panes[i].height, panes[i].width);
-
-    struct winsize ws;
-    ws.ws_row = panes[i].height;
-    ws.ws_col = panes[i].width;
-    ws.ws_xpixel = 0;
-    ws.ws_ypixel = 0;
-
-    if (ioctl(panes[i].process, TIOCSWINSZ, &ws) < 0) {
-      exit(EXIT_FAILURE);
-    }
-
     col += panes[i].width + 1;
     remaining -= panes[i].width;
   }
+
+  updateLayout(panes, nPanes);
 }
 
 void calculateLayout(Pane *panes, int nPanes, int height, int width) {
