f9a37b562adb5acc02c596d8b4fa3b9c397cf55e
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-04-29 21:03:18 -0500

    feat: Add data structures
    The previous version of ntmux had a pretty messy data layout. This is an
    attempt to make a more principled data structure that mimics how tmux
    works (Processes belong to Panes belong to Windows belong to Sessions).

---
+++ sockets/session.h
@@ -0,0 +1,47 @@
+#ifndef SESSION_H
+#define SESSION_H
+
+#include <vterm.h>
+
+typedef enum Layout {
+  LAYOUT_HORIZONTAL = 0,
+  LAYOUT_VERTICAL = 1,
+} Layout;
+
+typedef struct Process {
+  char *name;
+  int pid;
+  int fd;
+  VTerm *vt;
+  VTermScreen *vts;
+  bool closed;
+} Process;
+
+typedef struct Pane {
+  int row;
+  int col;
+  int width;
+  int height;
+  Process process;
+} Pane;
+
+typedef struct Window {
+  Pane *panes;
+  char *title;
+  int pane_count;
+  int current_pane;
+  Layout layout;
+} Window;
+
+typedef struct Session {
+  Window *windows;
+  char *title;
+  int window_count;
+  int current_window;
+} Session;
+
+void print_sessions(Session *sessions, int count);
+void add_pane(Window *window, int x, int y, int width, int height);
+Window *add_window(Session *session, char *title);
+
+#endif
