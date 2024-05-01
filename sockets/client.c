afdc8d98036d9864fe3ea6b6fee25bb3870f37ae
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-01 18:49:31 -0500

    feat: Add various control sequences to the client
    This commit is a bit messy, but it mostly adds key bindings, and enables
    mouse tracking.

--- sockets/client.c
+++ sockets/client.c
@@ -1,42 +1,58 @@
 #include <arpa/inet.h>
 #include <pthread.h>
+#include <readline/readline.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ioctl.h>
 #include <sys/select.h>
 #include <sys/socket.h>
 #include <termios.h>
 #include <unistd.h>
 
 struct termios term;
-enum Mode { MODE_NORMAL, MODE_CONTROL };
+enum Mode { MODE_NORMAL, MODE_CONTROL, MODE_CONTROL_STICKY };
 fd_set fds;
 
-bool ctrl_c_pressed = false;
-
+int ctrl_c_socket;
 void ctrl_c_callback(int sig) {
-  ctrl_c_pressed = true;
+  char buf[2];
+  buf[0] = 'e';
+  buf[1] = 3;
+  write(ctrl_c_socket, buf, 2);
 
   signal(sig, ctrl_c_callback);
+}
+
+void enable_mouse_tracking() {
+  write(STDOUT_FILENO, "\033[?1003h", 8); // Mouse tracking
+  //write(STDOUT_FILENO, "\033[?1006h", 8); // Extended mouse tracking
+}
+
+void disable_mouse_tracking() {
+  write(STDOUT_FILENO, "\033[?1003l", 8); // Mouse tracking
+  //write(STDOUT_FILENO, "\033[?1006l", 8); // Extended mouse tracking
 }
 
 void raw_mode() {
   tcgetattr(STDIN_FILENO, &term);
   term.c_lflag &= ~(ICANON | ECHO);
   tcsetattr(STDIN_FILENO, TCSANOW, &term);
+  enable_mouse_tracking();
 }
 
 void reset_mode() {
+  disable_mouse_tracking();
   term.c_lflag |= ICANON | ECHO;
   tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
 
-void configure_server(struct sockaddr_in *server) {
+void configure_server(struct sockaddr_in *server, int port) {
   server->sin_addr.s_addr = inet_addr("127.0.0.1");
   server->sin_family = AF_INET;
-  server->sin_port = htons(8888);
+  server->sin_port = htons(port);
 }
 
 int connect_to_server(int sock, struct sockaddr_in *server) {
@@ -67,7 +83,7 @@ void receive_message(int sock) {
     write(STDOUT_FILENO, server_reply, read_size);
     fflush(stdout);
   } else {
-    puts("Timeout");
+    puts("Timeout (This should not happen)");
   }
 }
 
@@ -77,6 +93,9 @@ void handle_key(int numRead, char *buf, int sock, char *str, char c) {
   }
 }
 
+// TODO: Delete this
+void send_size(int sock);
+
 void event_loop(int sock) {
   raw_mode();
 
@@ -84,7 +103,7 @@ void event_loop(int sock) {
   char buf[32];
   buf[0] = 'e';
   while (1) {
-    ssize_t numRead = read(STDIN_FILENO, buf + 1, 31);
+    ssize_t numRead = read(STDIN_FILENO, buf + 1, 6);
     if (numRead <= 0) {
       exit(EXIT_SUCCESS);
     }
@@ -95,9 +114,12 @@ void event_loop(int sock) {
       } else {
         write(sock, buf, numRead + 1);
       }
-    } else if (mode == MODE_CONTROL) {
+    } else if (mode == MODE_CONTROL || mode == MODE_CONTROL_STICKY) {
+      // TODO: Change to lua
       handle_key(numRead, buf, sock, "cSplit", '|');
       handle_key(numRead, buf, sock, "cVSplit", '_');
+      handle_key(numRead, buf, sock, "cSplit", '"');
+      handle_key(numRead, buf, sock, "cVSplit", '%');
       handle_key(numRead, buf, sock, "cList", 'i');
       handle_key(numRead, buf, sock, "cCreate", 'c');
       handle_key(numRead, buf, sock, "cReload", 'r');
@@ -105,6 +127,12 @@ void event_loop(int sock) {
       handle_key(numRead, buf, sock, "cDown", 'j');
       handle_key(numRead, buf, sock, "cUp", 'k');
       handle_key(numRead, buf, sock, "cRight", 'l');
+      handle_key(numRead, buf, sock, "cZoom", 'z');
+      handle_key(numRead, buf, sock, "cEven_Horizontal", '1');
+      handle_key(numRead, buf, sock, "cEven_Vertical", '2');
+      handle_key(numRead, buf, sock, "cMain_Horizontal", '3');
+      handle_key(numRead, buf, sock, "cMain_Vertical", '4');
+      handle_key(numRead, buf, sock, "cTiled", '5');
       handle_key(numRead, buf, sock, "cNext", 'n');
       handle_key(numRead, buf, sock, "cPrev", 'p');
 
