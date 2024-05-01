de9d6e02e4a8bfe975fc2e64bc103d821d73e455
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-01 18:42:33 -0500

    feat: Add functions for printing data structures
    These functions "pretty print" the data structures.
    
    These functions replace an earlier, sloppier, function.

--- sockets/session.c
+++ sockets/session.c
@@ -1,7 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "session.h"
+
+void print_layout(Layout layout) {
+  switch (layout) {
+  case LAYOUT_DEFAULT:
+    printf("Default");
+    break;
+  case LAYOUT_EVEN_HORIZONTAL:
+    printf("Even Horizontal");
+    break;
+  case LAYOUT_EVEN_VERTICAL:
+    printf("Even Vertical");
+    break;
+  case LAYOUT_MAIN_HORIZONTAL:
+    printf("Main Horizontal");
+    break;
+  case LAYOUT_MAIN_VERTICAL:
+    printf("Main Vertical");
+    break;
+  case LAYOUT_TILED:
+    printf("Tiled");
+    break;
+  default:
+    printf("Unknown");
+    break;
+  }
+}
+
+void print_cursor_shape(int cursor_shape) {
+  switch (cursor_shape) {
+  case 0:
+    printf("Default");
+    break;
+  case VTERM_PROP_CURSORSHAPE_BLOCK:
+    printf("Block");
+    break;
+  case VTERM_PROP_CURSORSHAPE_UNDERLINE:
+    printf("Underline");
+    break;
+  case VTERM_PROP_CURSORSHAPE_BAR_LEFT:
+    printf("Bar Left");
+    break;
+  default:
+    printf("Unknown");
+    break;
+  }
+}
+
+void print_process(Process *process) {
+  if (process->pid != -1) {
+    printf("      Process: %s\n", process->name);
+    printf("        pid: %d\n", process->pid);
+    printf("        fd: %d\n", process->fd);
+    printf("        closed: %s\n", process->closed ? "True" : "False");
+    printf("        cursor_visible: %s\n",
+           process->cursor_visible ? "True" : "False");
+    printf("        cursor_shape: ");
+    print_cursor_shape(process->cursor_shape);
+    printf("\n");
+  } else {
+    printf("      Process: %s\n", "None");
+  }
+}
+
+void print_pane(Pane *pane) {
+  printf("    Pane: %p\n", pane);
+  printf("      col: %d\n", pane->col);
+  printf("      row: %d\n", pane->row);
+  printf("      width: %d\n", pane->width);
+  printf("      height: %d\n", pane->height);
+  print_process(&pane->process);
+}
+
+void print_window(Window *window) {
+  printf("  Window: %s\n", window->title);
+  printf("    Pane Count: %d\n", window->pane_count);
+  printf("    Current Pane: %d\n", window->current_pane);
+  printf("    Layout: ");
+  print_layout(window->layout);
+  printf("\n");
+  for (int j = 0; j < window->pane_count; j++) {
+    Pane *pane = &window->panes[j];
+    print_pane(pane);
+  }
+}
 
 void print_session(Session *session) {
   printf("Session: %s\n", session->title);
