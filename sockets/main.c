866bcb3f58bd8d7170b3293fde2fe8a0a14b2f6b
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-02 06:05:27 -0500

    feat: Add usage statement and set the port from args
    This commit adds support for setting the port for the application when
    it is started.
    
    This also adds a more accurate usage statement.

--- sockets/main.c
+++ sockets/main.c
@@ -5,20 +5,33 @@
 #include "client.h"
 #include "server.h"
 
+void usage(const char *progname) {
+  fprintf(stderr, "Usage: %s server|client [port]\n", progname);
+}
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    printf("Usage: %s [server|client]\n", argv[0]);
+    usage(argv[0]);
     return EXIT_FAILURE;
   }
 
   int port = 8888;
+
+  if (argc > 2) {
+    port = atoi(argv[2]);
+    if (port <= 0 || port > 65535) {
+      fprintf(stderr, "Invalid port number: %s\n", argv[2]);
+      usage(argv[0]);
+      return EXIT_FAILURE;
+    }
+  }
 
   if (strcmp(argv[1], "server") == 0) {
     return server(port);
   } else if (strcmp(argv[1], "client") == 0) {
     return client(port);
   } else {
-    printf("Usage: %s [server|client]\n", argv[0]);
+    usage(argv[0]);
     return EXIT_FAILURE;
   }
 }
