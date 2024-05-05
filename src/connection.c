6dd1ac931ac4fc7934f657a857abdb53014f8dae
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-05 12:26:49 -0500

    feat: Add connection module
    This module adds some functions for handling server connections.
    
    This is a possible target for refactoring for cross-platform
    compatibility.

---
+++ src/connection.c
@@ -0,0 +1,33 @@
+#include <arpa/inet.h>
+#include <dirent.h>
+#include <lua5.4/lauxlib.h>
+#include <lua5.4/lua.h>
+#include <lua5.4/lualib.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+
+int accept_connection(int socket_desc, struct sockaddr_in client) {
+  int c = sizeof(struct sockaddr_in);
+  int socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
+  if (socket < 0) {
+    perror("accept failed");
+    exit(EXIT_FAILURE);
+  }
+  return socket;
+}
+
+void wait_for_connection(int socket_desc) {
+  listen(socket_desc, 3);
+  puts("Waiting for incoming connections...");
+}
+
+int *init_connection(int socket_desc) {
+  wait_for_connection(socket_desc);
+  struct sockaddr_in client;
+  int *socket = malloc(sizeof(*socket));
+  *socket = accept_connection(socket_desc, client);
+  return socket;
+}
---
+++ src/connection.h
@@ -0,0 +1,6 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+int *init_connection(int socket_desc);
+
+#endif
