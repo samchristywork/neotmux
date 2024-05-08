19ed96455924366f2e550d87a7c83a6d2e76b254
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-08 18:23:37 -0500

    feat: Implement more sophisticated mouse tracking
    We now properly deserialize and reserialize mouse events, this allows us
    to interpolate mouse event locations.

--- src/mouse.c
+++ src/mouse.c
@@ -1,9 +1,58 @@
-#include <stdio.h>
 #include <unistd.h>
 
+#include "command.h"
 #include "session.h"
 
 extern Neotmux *neotmux;
+
+typedef enum MouseEventType {
+  MOUSE_UNKNOWN = 0,
+  MOUSE_LEFT_CLICK = 32,
+  MOUSE_MIDDLE_CLICK = 33,
+  MOUSE_RIGHT_CLICK = 34,
+  MOUSE_RELEASE = 35,
+  MOUSE_LEFT_DRAG = 64,
+  MOUSE_MIDDLE_DRAG = 65,
+  MOUSE_MOVE = 67,
+  MOUSE_RIGHT_DRAG = 66,
+  MOUSE_WHEEL_DOWN = 96,
+  MOUSE_WHEEL_UP = 97,
+} MouseEventType;
+
+typedef struct MouseEvent {
+  MouseEventType type;
+  int x;
+  int y;
+} MouseEvent;
+
+bool deserialize_mouse_event(char *buf, int read_size, MouseEvent *event) {
+  if (read_size < 6) {
+    return false;
+  } else if (buf[0] != 0x1b || buf[1] != '[' || buf[2] != 'M') {
+    return false;
+  }
+
+  event->type = (unsigned char)buf[3];
+  event->x = (unsigned char)buf[4] - 0x20;
+  event->y = (unsigned char)buf[5] - 0x20;
+
+  return true;
+}
+
+bool serialize_mouse_event(char *buf, int buf_size, MouseEvent *event) {
+  if (buf_size < 6) {
+    return false;
+  }
+
+  buf[0] = 0x1b;
+  buf[1] = '[';
+  buf[2] = 'M';
+  buf[3] = event->type;
+  buf[4] = event->x + 0x20;
+  buf[5] = event->y + 0x20;
+
+  return true;
+}
 
 bool is_inside_rect(VTermRect rect, VTermPos pos) {
   return pos.col >= rect.start_col && pos.col < rect.end_col &&
@@ -11,49 +60,51 @@ bool is_inside_rect(VTermRect rect, VTermPos pos) {
 }
 
 bool handle_mouse(int socket, char *buf, int read_size) {
-  bool dirty = false;
-  fprintf(neotmux->log, "Mouse event (%d): ", socket);
+  if (read_size < 6) {
+    return false;
+  }
 
-  // Must be unsigned
-  int b = (unsigned char)buf[4] & 0x03;
-  int x = (unsigned char)buf[5] - 0x20;
-  int y = (unsigned char)buf[6] - 0x20;
+  MouseEvent event;
+  if (!deserialize_mouse_event(buf + 1, read_size - 1, &event)) {
+    return false;
+  }
 
-  fprintf(neotmux->log, "%d, %d, %d\n", x, y, b);
+  Window *w = get_current_window(neotmux);
+  if (w->zoom == -1) {
+    if (event.type == MOUSE_LEFT_CLICK) {
+      for (int i = 0; i < w->pane_count; i++) {
+        if (w->current_pane == i) {
+          continue;
+        }
 
-  if (b == 0) {
-    fprintf(neotmux->log, "Left click\n");
-    Window *w = get_current_window(neotmux);
-    for (int i = 0; i < w->pane_count; i++) {
-      fprintf(neotmux->log, "Checking pane %d\n", i);
-      fprintf(neotmux->log, "  %d, %d, %d, %d\n", w->panes[i].col, w->panes[i].row,
-             w->panes[i].width, w->panes[i].height);
-      Pane *p = &w->panes[i];
-      VTermRect rect = {.start_col = p->col,
-                        .start_row = p->row,
-                        .end_col = p->col + p->width,
-                        .end_row = p->row + p->height};
-      VTermPos pos = {.col = x - 1, .row = y - 1};
-      if (is_inside_rect(rect, pos)) {
-        fprintf(neotmux->log, "Found pane %d\n", i);
-        w->current_pane = i;
-        dirty = true;
-        break;
+        Pane *p = &w->panes[i];
+        VTermRect rect = {.start_col = p->col,
+                          .start_row = p->row,
+                          .end_col = p->col + p->width,
+                          .end_row = p->row + p->height};
+        VTermPos pos = {.col = event.x - 1, .row = event.y - 1};
+        if (is_inside_rect(rect, pos)) {
+          w->current_pane = i;
+          return true;
+        }
       }
     }
   }
 
   Pane *pane = get_current_pane(neotmux);
-
-  // VTERM_PROP_MOUSE_NONE = 0,
-  // VTERM_PROP_MOUSE_CLICK,
-  // VTERM_PROP_MOUSE_DRAG,
-  // VTERM_PROP_MOUSE_MOVE,
-  // TODO: Handle all mouse states
   if (pane->process->cursor.mouse_active != VTERM_PROP_MOUSE_NONE) {
-    write(pane->process->fd, buf + 1, read_size - 1);
-    // TODO: This triggers a redraw, which we might not want.
+    event.x -= pane->col;
+    event.y -= pane->row;
+
+    char buf[6];
+    if (serialize_mouse_event(buf, 6, &event)) {
+      if (event.type == MOUSE_LEFT_CLICK) {
+        write(pane->process->fd, buf, 6);
+        return true;
+      }
+      // TODO: Implement other mouse events
+    }
   }
 
-  return dirty;
+  return false;
 }
