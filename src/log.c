1fc741fd14d2370f5fb925448d4acdbf40578f82
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-16 20:42:49 -0500

    feat: Simplify module name in log output
    The module name used to look like this:
    
    ```
    src/command.c
    ```
    
    but now it looks like this:
    
    ```
    command
    ```

--- src/log.c
+++ src/log.c
@@ -5,16 +5,40 @@
 
 extern Neotmux *neotmux;
 
-void write_log(char *module, int line, int socket, char *msg) {
+char *normalize_module_name(char *module) {
+  // Strip leading path
+  char *last_slash = strrchr(module, '/');
+  if (last_slash) {
+    module = last_slash + 1;
+  }
+
+  char *ret = strdup(module);
+
+  // Strip trailing .c
+  char *last_dot = strrchr(ret, '.');
+  if (last_dot) {
+    last_dot[0] = '\0';
+  }
+
+  return ret;
+}
+
+void write_log(char *type, char *module, int line, int socket, char *msg) {
   const char *yellow = "\033[33m";
   const char *grey = "\033[90m";
   const char *blue = "\033[34m";
   const char *reset = "\033[0m";
 
+  module = normalize_module_name(module);
+
   char str[20];
   time_t now = time(NULL);
   struct tm *t = localtime(&now);
   strftime(str, sizeof(str), "%H:%M:%S", t);
-  fprintf(neotmux->log, "INFO %s%s %s%d %s%3d %s%s%s: %s\n", grey, str, blue,
-          socket, grey, line, yellow, module, reset, msg);
+  fprintf(neotmux->log, "%s %s%s %s%d %s%3d %s%s%s: %s\n", type, grey, str,
+          blue, socket, grey, line, yellow, module, reset, msg);
+
+  free(module);
+
+  fflush(neotmux->log);
 }
--- src/log.h
+++ src/log.h
@@ -1,13 +1,14 @@
 #ifndef LOG_H
 #define LOG_H
 
-void write_log(char *module, int line, int socket, char *msg);
+void write_log(char *type, char *module, int line, int socket, char *msg);
 
+// TODO: Change to LOG_INFO
 #define WRITE_LOG(socket, fmt, ...)                                            \
   {                                                                            \
     char *str;                                                                 \
     asprintf(&str, fmt, ##__VA_ARGS__);                                        \
-    write_log(__FILE__, __LINE__, socket, str);                                \
+    write_log("INFO", __FILE__, __LINE__, socket, str);                        \
     free(str);                                                                 \
   }
 
