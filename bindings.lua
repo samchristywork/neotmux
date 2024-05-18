89c761e9fb7ea14a049ff9ba67c8393bd6c811bf
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-18 15:20:40 -0500

    feat: Rework bindings
    Bindings are now no longer just a bunch of if..else if..else statements,
    they are part of a data structure. This makes it much easier for foreign
    plugins to work with it.

--- bindings.lua
+++ bindings.lua
@@ -1,89 +1,81 @@
-function handle_binding_normal(buf)
-  -- Alt + hjkl
-  if (buf == "\x1bh") then
-    return "cLeft"
-  elseif(buf == "\x1bj") then
-    return "cDown"
-  elseif(buf == "\x1bk") then
-    return "cUp"
-  elseif(buf == "\x1bl") then
-    return "cRight"
-  elseif(buf == "\x1bH") then
-    return "cSwapLeft"
-  elseif(buf == "\x1bJ") then
-    return "cSwapDown"
-  elseif(buf == "\x1bK") then
-    return "cSwapUp"
-  elseif(buf == "\x1bL") then
-    return "cSwapRight"
-  end
+local bindings = {
+  ["control"] = {},
+  ["normal"] = {}
+}
 
-  if(buf == "\x1bn") then
-    return "cNext"
-  elseif(buf == "\x1bp") then
-    return "cPrev"
-  elseif (buf == "\x1bz") then
-    return "cZoom"
+-- TODO: Should throw error on syntax error
+function handle_binding_normal(sock, buf)
+  for key, value in pairs(bindings["normal"]) do
+    if (buf == key) then
+      write_string(sock, value)
+      return true
+    end
   end
-
-  return NULL
+  return false
 end
 
-function handle_binding_control(buf)
-  -- if (buf == "\x1b[5~") then
-    -- return "cScrollUp" --elseif(buf == "\x1b[6~") then
-    -- return "cScrollDown"
-  if (buf == "\x1b[D") then
-    return "cLeft"
-  elseif(buf == "\x1b[C") then
-    return "cRight"
-  elseif(buf == "\x1b[A") then
-    return "cUp"
-  elseif(buf == "\x1b[B") then
-    return "cDown"
-  elseif(buf == "|") then
-    return "cSplit"
-  elseif(buf == "_") then
-    return "cVSplit"
-  elseif(buf == "\"") then
-    return "cSplit"
-  elseif(buf == "%") then
-    return "cVSplit"
-  elseif(buf == "i") then
-    return "cList"
-  elseif(buf == "y") then
-    return "cCycleStatus"
-  elseif(buf == "e") then
-    return "cCreate"
-  elseif(buf == "r") then
-    return "cReloadLua"
-  elseif(buf == "h") then
-    return "cLeft"
-  elseif(buf == "j") then
-    return "cDown"
-  elseif(buf == "k") then
-    return "cUp"
-  elseif(buf == "l") then
-    return "cRight"
-  elseif(buf == "z") then
-    return "cZoom"
-  elseif(buf == "1") then
-    return "cEven_Horizontal"
-  elseif(buf == "2") then
-    return "cEven_Vertical"
-  elseif(buf == "3") then
-    return "cMain_Horizontal"
-  elseif(buf == "4") then
-    return "cMain_Vertical"
-  elseif(buf == "5") then
-    return "cTiled"
-  elseif(buf == "6") then
-    return "cCustom"
-  elseif(buf == "n") then
-    return "cNext"
-  elseif(buf == "p") then
-    return "cPrev"
+function handle_binding_control(sock, buf)
+  for key, value in pairs(bindings["control"]) do
+    if (buf == key) then
+      write_string(sock, value)
+      return true
+    end
   end
-
-  return NULL
+  return false
 end
+
+-- Alt + hjkl
+bindings["normal"]["\x1bh"] = "cLeft"
+bindings["normal"]["\x1bj"] = "cDown"
+bindings["normal"]["\x1bk"] = "cUp"
+bindings["normal"]["\x1bl"] = "cRight"
+
+-- Alt + HJKL
+bindings["normal"]["\x1bH"] = "cSwapLeft"
+bindings["normal"]["\x1bJ"] = "cSwapDown"
+bindings["normal"]["\x1bK"] = "cSwapUp"
+bindings["normal"]["\x1bL"] = "cSwapRight"
+
+-- Misc.
+bindings["normal"]["\x1bn"] = "cNext"
+bindings["normal"]["\x1bp"] = "cPrev"
+bindings["normal"]["\x1bz"] = "cZoom"
+
+-- Directional
+bindings["control"]["\x1b[D"] = "cLeft"
+bindings["control"]["\x1b[C"] = "cRight"
+bindings["control"]["\x1b[A"] = "cUp"
+bindings["control"]["\x1b[B"] = "cDown"
+
+-- Splits
+bindings["control"]["\x1b|"] = "cSplit"
+bindings["control"]["\x1b_"] = "cVSplit"
+bindings["control"]["\x1b\""] = "cSplit"
+bindings["control"]["\x1b%"] = "cVSplit"
+
+-- Layout
+bindings["control"]["1"] = "cEven_Horizontal"
+bindings["control"]["2"] = "cEven_Vertical"
+bindings["control"]["3"] = "cMain_Horizontal"
+bindings["control"]["4"] = "cMain_Vertical"
+bindings["control"]["5"] = "cTiled"
+bindings["control"]["6"] = "cCustom"
+
+-- Directional
+bindings["control"]["h"] = "cLeft"
+bindings["control"]["j"] = "cDown"
+bindings["control"]["k"] = "cUp"
+bindings["control"]["l"] = "cRight"
+
+-- Window
+bindings["control"]["e"] = "cCreate"
+bindings["control"]["n"] = "cNext"
+bindings["control"]["p"] = "cPrev"
+
+-- Misc.
+bindings["control"]["i"] = "cList"
+bindings["control"]["y"] = "cCycleStatus"
+bindings["control"]["r"] = "cReloadLua"
+bindings["control"]["z"] = "cZoom"
+
+-- TODO: Page Up and Page Down
