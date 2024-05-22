ed92acbe98d215d276015e9a79e6b0694e4531be
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-22 18:06:33 -0500

    feat: Break key bindings into a table
    Tables can have extra metadata associated with them. In this case, each
    binding now has a command and a doc string.

--- bindings/handle.lua
+++ bindings/handle.lua
@@ -1,10 +1,11 @@
 function handle_binding_normal(sock, buf)
   for key, value in pairs(bindings["normal"]) do
     if (buf == key) then
-      if (type(value) == "function") then
-        value(sock)
+      local command = value.command
+      if (type(command) == "function") then
+        command(sock)
       else
-        write_string(sock, value)
+        write_string(sock, command)
       end
       return true
     end
@@ -15,10 +16,11 @@ end
 function handle_binding_control(sock, buf)
   for key, value in pairs(bindings["control"]) do
     if (buf == key) then
-      if (type(value) == "function") then
-        value(sock)
+      local command = value.command
+      if (type(command) == "function") then
+        command(sock)
       else
-        write_string(sock, value)
+        write_string(sock, command)
       end
       return true
     end
