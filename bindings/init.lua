276a34619af9b1b8eab877781e8579453687738c
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-05-22 19:50:47 -0500

    feat: Construct bindings with a function
    Previously, we were editing the raw bindings table. Now, we add to
    bindings using a function instead so that we can enforce a specific
    format.

--- bindings/init.lua
+++ bindings/init.lua
@@ -10,70 +10,81 @@ bindings = {
 
 local alt = "\x1b"
 
-local normal = bindings["normal"]
-local control = bindings["control"]
+function add_binding(mode, key, value, docs)
+  local normal = bindings["normal"]
+  local control = bindings["control"]
+  if mode == "n" then
+    normal[key] = {}
+    normal[key].command = value
+    normal[key].docs = docs
+  elseif mode == "c" then
+    control[key] = {}
+    control[key].command = value
+    control[key].docs = docs
+  end
+end
 
 -- Alt + hjkl
-normal[alt.."h"] = Command("Left")
-normal[alt.."j"] = function(sock) write_string(sock, Command("Down")) end
-normal[alt.."k"] = Command("Up")
-normal[alt.."l"] = Command("Right")
+add_binding("n", alt.."h", Command("Left"), "Move focus left")
+add_binding("n", alt.."j", function(sock) write_string(sock, Command("Down")) end, "Move focus down")
+add_binding("n", alt.."k", Command("Up"), "Move focus up")
+add_binding("n", alt.."l", Command("Right"), "Move focus right")
 
 -- Alt + HJKL
-normal[alt.."H"] = Command("SwapLeft")
-normal[alt.."J"] = Command("SwapDown")
-normal[alt.."K"] = Command("SwapUp")
-normal[alt.."L"] = Command("SwapRight")
+add_binding("n", alt.."H", Command("SwapLeft"), "Swap pane left")
+add_binding("n", alt.."J", Command("SwapDown"), "Swap pane down")
+add_binding("n", alt.."K", Command("SwapUp"), "Swap pane up")
+add_binding("n", alt.."L", Command("SwapRight"), "Swap pane right")
 
 -- Misc.
-normal["\x0a"] = Event("\x0d") -- Convert newline to carriage return
-normal[alt.."?"] = Command("Help")
-normal[alt.."n"] = Command("Next")
-normal[alt.."p"] = Command("Prev")
-normal[alt.."z"] = Command("Zoom")
+add_binding("n", "\x0a", Event("\x0d"), nil) -- Convert newline to carriage return
+add_binding("n", alt.."n", Command("Next"), "Next window")
+add_binding("n", alt.."p", Command("Prev"), "Previous window")
+add_binding("n", alt.."z", Command("Zoom"), "Zoom window")
 
 -- Directional
-control["\x1b[A"] = Command("Up")
-control["\x1b[B"] = Command("Down")
-control["\x1b[C"] = Command("Right")
-control["\x1b[D"] = Command("Left")
+add_binding("c", "\x1b[A", Command("Up"), "Move focus up")
+add_binding("c", "\x1b[B", Command("Down"), "Move focus down")
+add_binding("c", "\x1b[C", Command("Right"), "Move focus right")
+add_binding("c", "\x1b[D", Command("Left"), "Move focus left")
 
 -- Splits
 -- TODO: There is currently no distinction between horizontal and vertical splits
-control["|"] = Command("Split")
-control["_"] = Command("VSplit")
-control["\""] = Command("Split")
-control["%"] = Command("VSplit")
+add_binding("c", "|", Command("Split"), "Split horizontally")
+add_binding("c", "_", Command("VSplit"), "Split vertically")
+add_binding("c", "\"", Command("Split"), "Split horizontally")
+add_binding("c", "%", Command("VSplit"), "Split vertically")
 
 -- Layout
-control["1"] = Command("Layout layout_even_horizontal")
-control["2"] = Command("Layout layout_even_vertical")
-control["3"] = Command("Layout layout_main_horizontal")
-control["4"] = Command("Layout layout_main_vertical")
-control["5"] = Command("Layout layout_tiled")
-control["6"] = Command("Layout layout_custom")
+add_binding("c", "1", Command("Layout layout_even_horizontal"), "Even horizontal layout")
+add_binding("c", "2", Command("Layout layout_even_vertical"), "Even vertical layout")
+add_binding("c", "3", Command("Layout layout_main_horizontal"), "Main horizontal layout")
+add_binding("c", "4", Command("Layout layout_main_vertical"), "Main vertical layout")
+add_binding("c", "5", Command("Layout layout_tiled"), "Tiled layout")
+add_binding("c", "6", Command("Layout layout_custom"), "Custom layout")
 
 -- Directional
-control["h"] = Command("Left")
-control["j"] = Command("Down")
-control["k"] = Command("Up")
-control["l"] = Command("Right")
+add_binding("c", "h", Command("Left"), "Move focus left")
+add_binding("c", "j", Command("Down"), "Move focus down")
+add_binding("c", "k", Command("Up"), "Move focus up")
+add_binding("c", "l", Command("Right"), "Move focus right")
 
 -- Window
-control["e"] = Command("Create")
-control["n"] = Command("Next")
-control["p"] = Command("Prev")
+add_binding("c", "e", Command("Create"), "Create window")
+add_binding("c", "n", Command("Next"), "Next window")
+add_binding("c", "p", Command("Prev"), "Previous window")
 
 -- Misc.
-control["\x01"] = Event("\x01") -- Ctrl + A
-control["\x02"] = Event("ls\n") -- Ctrl + B
-control["i"] = Command("List")
-control["q"] = Command("Quit")
-control["r"] = Command("ReloadLua")
-control["x"] = Command("CopySelection")
-control["y"] = Command("CycleStatus")
-control["z"] = Command("Zoom")
+add_binding("c", "\x01", Event("\x01"), nil) -- Ctrl + A
+add_binding("c", "\x02", Event("ls\n"), nil) -- Ctrl + B
+add_binding("c", "i", Command("List"), "List client data")
+add_binding("c", "q", Command("Quit"), "Quit")
+add_binding("c", "r", Command("ReloadLua"), "Reload Lua")
+add_binding("c", "x", Command("CopySelection"), "Copy selection")
+add_binding("c", "y", Command("CycleStatus"), "Cycle status")
+add_binding("c", "z", Command("Zoom"), "Zoom pane")
 
 -- TODO: Page Up and Page Down
 
 util.print_unused_bindings(bindings)
+util.list_docs(bindings)
